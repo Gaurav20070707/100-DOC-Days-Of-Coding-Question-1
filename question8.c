33309882ffcb55356f0d1b24a729afb1373e66ac
Gaurav20070707 <gaurav210707@gmail.com>
2025-08-26 17:53:48 +0530

    Create question8.c

---
+++ question8.c
@@ -0,0 +1,14 @@
+//Q8)Write a program to find and display the sum of the first n natural numbers.
+#include<stdio.h>
+
+int main() {
+
+int a;
+
+printf("enter a ",a);
+scanf("%d",&a);
+
+printf("sum of first a natural numbers is : %d\n",a*(a+1)/2);
+
+return 0; 
+}
