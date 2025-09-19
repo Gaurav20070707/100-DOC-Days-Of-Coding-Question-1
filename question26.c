63b8105aea5f2bc77e03a0bef9af765aae0a14a7
Gaurav20070707 <gaurav210707@gmail.com>
2025-09-19 10:21:51 +0530

    Create question26.c

---
+++ question26.c
@@ -0,0 +1,16 @@
+// Q26) Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+int a;
+printf("Enter a");
+scanf("%d",&a);
+int i;
+for (i=1;i<=a;i++) {
+
+    printf("%d\t",i);
+}
+
+    return 0;
+}
